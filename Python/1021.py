f3fc66d7a90d500d4dcbd167638aa99caf3afd3d
Mateus Takamatsu <mateus.takamatsu.dev@gmail.com>
2024-08-21 20:07:07 -0300

    Finished problem 1021 in Python

--- Python/1021.py
+++ Python/1021.py
@@ -1,17 +1,18 @@
 total = float(input())
-i = -1
-q = 0
-notasPossiveis = [100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.05, 0.01]
-nota = notasPossiveis[i]
+notasPossiveis = [100, 50, 20, 10, 5, 2]
+moedasPossiveis = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
 
-# Lógica funcionando, falta apenas ajustar a formatação
+def quantosDaUnidade(tipo,unidadesPossiveis,total):
+    i = -1
+    print(f'{tipo.upper()}S:')
+    while i < len(unidadesPossiveis)-1: # Faz o loop para cada unidade
+        q = 0
+        i+=1
+        unidade = unidadesPossiveis[i]
+        while round(total,2) >= unidade: # Faz o loop para cada q em uma unidade
+            total = total - unidade
+            q+=1
+        print(f'{q} {tipo}(s) de R$ {unidade:.2f}')
+    return(total)
 
-while i < len(notasPossiveis)-1:
-    q = 0
-    i+=1
-    nota = notasPossiveis[i]
-
-    while total >= nota:
-        total = total - nota
-        q+=1
-    print(f'{q} nota(s) de R$ {nota},00')
+quantosDaUnidade('moeda',moedasPossiveis,quantosDaUnidade('nota',notasPossiveis,total))
