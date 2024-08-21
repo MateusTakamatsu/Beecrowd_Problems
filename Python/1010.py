af8ab581076827faad69947eb208a0c334cb3ec7
Mateus Takamatsu <mateus.takamatsu.dev@gmail.com>
2024-08-21 12:59:16 -0300

    Optimizating problem 1010 in Python

--- Python/1010.py
+++ Python/1010.py
@@ -1,12 +1,10 @@
-linha1 = input()
-linha2 = input()
-array1 = linha1.split()
-array2 = linha2.split()
+peca1 = input().split()
+num1 = int(peca1[1])
+valor1 = float(peca1[2])
 
-num1 = int(array1[1])
-valor1 = float(array1[2])
-num2 = int(array2[1])
-valor2 = float(array2[2])
+peca2 = input().split()
+num2 = int(peca2[1])
+valor2 = float(peca2[2])
 
 preco = (num1*valor1) + (num2*valor2)
 
