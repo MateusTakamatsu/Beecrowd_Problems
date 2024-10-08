01751e9f5e1969f6bcde2a83b81c83d047564ea8
Mateus Takamatsu <mateus.takamatsu.dev@gmail.com>
2024-10-08 13:49:28 -0300

    style: format Ruby problem 1037

--- Ruby/1_Iniciante/1037.rb
+++ Ruby/1_Iniciante/1037.rb
@@ -3,7 +3,7 @@ num = gets.to_f.round(2)
 piso = 100
 intervalos = ['Fora de intervalo','Intervalo (75,100]','Intervalo (50,75]','Intervalo (25,50]','Intervalo [0,25]','Fora de intervalo']
 
-intervalos.length().times() do |i|
+intervalos.length().times do |i|
     if (num > piso) or (piso==0 and 0==num) or (piso==-25 and num<0)
         puts intervalos[i]
         break
